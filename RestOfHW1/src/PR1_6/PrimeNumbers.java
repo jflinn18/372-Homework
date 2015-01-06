f91a1993cce0f55318dd37600656feed8a60426f
jflinn18 <joeflinn412@gmail.com>
2015-01-05 21:02:38 -0800

    Added the exercises 1.2, 1.3, 1.4, 1.5
    I finished these exercises. I started 1.6, but I am having a hard time
    with the math.

---
+++ RestOfHW1/src/PR1_6/PrimeNumbers.java
@@ -0,0 +1,27 @@
+package PR1_6;
+import java.util.*;
+
+public class PrimeNumbers {
+
+	public static void main(String[]  args)
+	{
+		Scanner input = new Scanner(System.in);
+		
+		System.out.printf("Enter the upper bound: ");
+		int upperbound = input.nextInt();
+		
+		
+		for (int i = 0; i < i; i++)
+		{
+			for (int j = 1; j < i; j++)
+			{
+				if ()
+				System.out.println(i + " is a prime.");
+			}
+		}
+		
+		System.out.println("Done...................\n");
+
+		input.close();
+	}
+}
